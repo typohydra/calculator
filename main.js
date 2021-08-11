eecf016fcc02bf8073a7aed3882010fbea30f69c
typohydra <74554736+typohydra@users.noreply.github.com>
2021-08-11 20:46:47 +0400

    add main.js

---
+++ main.js
@@ -0,0 +1,37 @@
+function add(a, b) {
+    return a+b;
+}
+
+function subtract(a, b) {
+    return a-b;  
+}
+
+function multiply(a, b) {
+    return a*b;
+}
+
+function divide(a, b) {
+    return a/b;
+}
+
+function operate(operator, a, b) {
+    switch(operator) {
+        case '+':
+            add(a, b);
+            break;
+        case '-':
+            subtract(a, b);
+            break;
+        case 'x':
+            multiply(a, b);
+            break;
+        case '/':
+            divide(a, b);
+            break;
+    }
+}
+
+function display() {
+    
+}
+
